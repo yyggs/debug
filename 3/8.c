01e4d49d0d6d64366b25c3149ee3caece2d4e007
Terrence-zzh <1225734058@qq.com>
2020-06-30 23:55:31 +0800

    MODIFY 3

--- 3/8.c
+++ 3/8.c
@@ -7,63 +7,49 @@ int main(){
     int i,j;
     int count=0;
     int max;
-    
-    //input m & n & matrix
-    //printf("Enter the row & column of the matrix:");
-	scanf("%d %d",&m,&n);
-	//printf("\nEnter values in the matrix:\n");
-	for(i=0;i<m;i++){
-		for(j=0;j<n;j++){
-			scanf("%d",&matrix[i][j]);
-			if(j<n-1)scanf(" ");
+	scanf("%d %d", &m, &n);
+	for (i  = 0; i < m; i++) {
+		for (j = 0; j < n; j++) {
+			scanf("%d", &matrix[i][j]);
+			if (j < (n - 1)) {
+				scanf(" ");
+			}
 		}
 	}
-	max=m*n;
-	
-	//output the first line
-	//printf("\nThe first line of the original matrix is:");
-	for(j=0;j<n;j++){
-		printf("%d ",matrix[0][j]);
+	max = m * n;
+	for (j = 0; j < n; j++) {
+		if (m == 1 && j == n - 1) {
+            printf("%d", matrix[0][j]);
+        } else {
+		    printf("%d ", matrix[0][j]);
+        }
 		count++;
 	}
-	//printf("\n");
 		
-	do{	
-		//printf("\nm=%d\nn=%d",m,n);//for debug
-		int matrix_[n][m];//rotate the matrix to -90 degree which becomes matrix_
-		//printf("\nDelete the first line,then rotate the matrix -90 degree.The remaining matrix_ is:\n");
-		for(i=0;i<n;i++){
-			for(j=0;j<m-1;j++){
-				matrix_[i][j]=matrix[j+1][n-1-i];
-				//printf("%d ",matrix_[i][j]);
+	do {	
+		int matrix_[n][m];
+		for (i = 0;i < n; i++) {
+			for (j = 0; j < (m - 1); j++) {
+				matrix_[i][j] = matrix[j+1][n-1-i];
 			}
-			//printf("\n");
 		}
-		
-		/*if(m==1||n==1){
-			break;
-		}*/
-		//printf("\nThe first line of the rotated matrix_ is:");
-		for(j=0;j<m-1;j++){
-			printf("%d",matrix_[0][j]);
+		for (j = 0; j < (m - 1); j++) {
+			printf("%d", matrix_[0][j]);
 			count++;
-			if(count<max)printf(" ");
-		}
-	
-		//cover original matrix with new matrix_	
-		for(i=0;i<n;i++){
-			for(j=0;j<m;j++){
-				matrix[i][j]=matrix_[i][j];
+			if(count < max) {
+				printf(" ");
+			}
+		}	
+		for(i = 0; i < n; i++){
+			for(j = 0; j < m; j++){
+				matrix[i][j] = matrix_[i][j];
 			}
 		}
-		
 		int t;
 		t=m;
 		m=n;
-		n=t-1;
-		//m=n;n=m-1; no use t.This is so wrong!!!
-			
-	}while(m>=1&&n>=1);
+		n=t-1;	
+	} while (m >= 1 && n >= 1);
 	
     return 0;
 }
