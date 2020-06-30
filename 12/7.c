71611c1c55d123b884d02b719d9566b5300085f9
Terrence-zzh <1225734058@qq.com>
2020-07-01 00:29:58 +0800

    MODIFY 12

--- 12/7.c
+++ 12/7.c
@@ -2,11 +2,11 @@
 #include<math.h>
 #define PI 3.14159
 int main() {
-float r,c,s;
-    scanf("%lf\n",&r);
-    c=2*PI*r;
-        s=PI*r*r;
-    printf("%.3f\n",c);
- printf("%.3f\n",s);
+    double r, c, s;
+    scanf("%lf\n", &r);
+    c = 2 * PI * r;
+    s = PI * r * r;
+    printf("%.3lf\n", c);
+    printf("%.3lf\n", s);
     return 0;
 }
