98cf5c1fd23aefac236ec2044e1c9fc704a026d7
Terrence-zzh <1225734058@qq.com>
2020-07-01 00:25:55 +0800

    MODIFY 10

--- 10/6.c
+++ 10/6.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+
 int main() {
-int a,b,c;
-    scanf("%d",&c);
-        
-     for(b=2;b<=c;b++) { 
-     for(a=2;a<b;a++){
-     if(b%a==0){printf("");break;}
-     }
-        if (a>=b){printf("%d\n"),b;}
+    int a, b, c;
+    scanf("%d", &c);
+    for (b = 2; b <= c; b++) { 
+        for (a = 2; a < b; a++) {
+            if (b % a == 0) {
+            	printf("");
+            	break;
+            }
+        }
+        if (a >= b) {
+        	printf("%d\n", b);
+        }
      }
     return 0;
 }
